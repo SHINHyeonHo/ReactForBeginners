c3bc2dd7679181f4cbb63f3dbd7a5934d1801309
hyeonhoS <hyunho2005@naver.com>
2025-06-28 10:33:19 +0900

    #7.3 Movie App react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import ToDo from "./ToDo";
-import CoinTracker from "./CoinTracker";
+import ToDo from "./components/ToDo";
+import CoinTracker from "./components/CoinTracker";
+import MovieApp from "./components/MovieApp";
 
 function App() {
-  return <CoinTracker />;
+  return <MovieApp />;
 }
 
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+
+function Movie({ key, coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>{title}</h2>
+      <p>{summary ?? "No summary available."}</p>
+      {genres && Array.isArray(genres) && genres.length > 0 ? (
+        <ul>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
